d79e1e3b5290765730ceab1f2c611e978db1176d
Scott Griffin <scottgriffinm@gmail.com>
2023-09-05 01:52:30 -0500

    sdg

--- data/lyrics/prepare.py
+++ data/lyrics/prepare.py
@@ -8,6 +8,21 @@ input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 with open(input_file_path, 'r', encoding='utf-8') as f:
     data = f.read()
 n = len(data)
+
+
+def split_data(data_path,n):
+    with open(data_path, 'r', encoding='utf-8') as f:
+        data = f.read()
+    data_len = len(data)
+    split_data = []
+    for i in range(n):
+        split_data.append(data[int(i*(data_len/n)):int((i+1)*(data_len/n))])
+    for i in range(len(split_data)):
+        with open('split/' + 'input' + str(i) + '.txt', 'w', encoding='utf-8') as f:
+            f.write(split_data[i])
+
+
+
 train_data = data[:int(n*0.9)]
 val_data = data[int(n*0.9):]
 
